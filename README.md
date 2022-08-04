de32fcb3f2ecb83bdf77a13bd66bfcb728c2b63c
Brandan Delafuente <brandandelafuente@gmail.com>
2022-08-03 21:51:32 -0400

    Added README.md, modified guac.lua to add credits

---
+++ README.md
@@ -0,0 +1,7 @@
+## Moonbase CVM
+**a Lua CollabVM Implementation**
+
+Once this project is done, you'll just have a nice and fancy chat client for CollabVM in Lua :)
+
+- [X] Guacamole implementation (Thanks @MDMCK10)
+- [] CollabVM implementation  
--- guac.lua
+++ guac.lua
@@ -1,16 +1,6 @@
--- local str = string
--- guacamole = {
--- parse = function(string)
- 
--- local pos,sect = -1,{};
-
--- while wait() do
--- local len=str.find(".",nil,pos+1)
-
--- if len == -1 then break
-
--- pos=
-
+--[[
+ guac utilization implementation by MDMCK10
+]]--
 function decodeResponse(a)
     local pos = -1;
     local sections = {};
